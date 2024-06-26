57def6848300d28dd474bd8ff069412839f3383b
Bhaskar Rijal <bhaskarrijal13@gmail.com>
2024-06-26 12:55:44 +0545

    chore: Update stylesheet path to use absolute URL

--- packages/blog-starter-kit/themes/personal/pages/_app.tsx
+++ packages/blog-starter-kit/themes/personal/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
-import '../styles/index.css';
+// import '../styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
